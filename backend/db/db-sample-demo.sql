48b10a374f6af57b63db6a8dcbb291f7dbfad016
HofmannCh <lukas-hofmann@gmx.ch>
2020-06-16 08:26:09 +0200

    Update demo data

--- backend/db/db-sample-demo.sql
+++ backend/db/db-sample-demo.sql
@@ -1,14 +1,14 @@
 INSERT INTO "user" (email, password, salaryDay) VALUES
-('demo@test.com', '123456', 28);
+('demo2@test.com', '123456', 28);
 
 INSERT INTO "category" (userId, name, budget) VALUES
-(1, 'Essen', 30),
-(1, 'Sparen', 60);
+(1, 'Essen', null),
+(1, 'Sparen', null);
 
 INSERT INTO "transaction" (categoryId, description, amount, createdAt) VALUES
 (1, 'Essensgeld', 50.00,'2020-06-05'),
-(1, 'Burger', -12.50,'2020-06-06'),
-(1, 'Nachtessen', -20.30,'2020-06-11'),
+(1, 'Burger', -13.00,'2020-06-06'),
+(1, 'Nachtessen', -21.00,'2020-06-11'),
 (1, 'Essensgeld', 40.00,'2020-06-19'),
 (2, 'Sackgeld', 20.00,'2020-06-08'),
 (2, 'Sackgeld', 20.00,'2020-06-15'),
